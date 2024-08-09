a7383dd5ba5a74d99398664fa1bd8a9d344c778e
lanceu0128 <lanceu0128@gmail.com>
2024-08-08 21:16:32 -0400

    fixed AI profile pic

--- app/components/ChatMessageView.tsx
+++ app/components/ChatMessageView.tsx
@@ -3,6 +3,7 @@
 import { useRef, useEffect, ReactElement } from 'react';
 import { Message, Role } from '../types';
 import Image from 'next/image';
+import pfp from '../images/pfp.jpg';
 
 interface ChatMessageViewProps {
     messages: Message[];
@@ -26,13 +27,13 @@ const ChatMessageView = ( {messages}: ChatMessageViewProps ): ReactElement => {
                     </h1>
                 ) : (
                     <div key={index} className="flex items-start mb-5">
-                        <div className="w-10 h-10 flex-shrink-0 mr-3">
+                        <div className="w-10 h-10 flex-shrink-0 mr-3 relative">
                             <Image 
-                                src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/6b483e19159771.562d5d6d738ba.png" 
-                                alt="Your image description"
+                                src={pfp}
+                                alt="AI Pfp"
                                 layout="fill" 
                                 objectFit="cover"
-                                className="w-full h-full"
+                                className="rounded-full"
                             />
                         </div>
                         <h1 className="font-medium text-lg px-4 py-2 rounded-3xl flex-grow"> 
